c9a29dcb53ab6ff1afd538ca725e6f83504f3b59
beny2783 <benny2783@gmail.com>
2025-02-14 17:18:16 +0000

    running correctly

---
+++ metro.config.js
@@ -0,0 +1,8 @@
+/*
+ * Metro configuration for React Native
+ * https://docs.expo.dev/guides/customizing-metro/
+ */
+
+const { getDefaultConfig } = require('expo/metro-config');
+
+module.exports = getDefaultConfig(__dirname);
